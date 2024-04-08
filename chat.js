dd15b48efffb7a8c7c844c2bff88c63d84642319
Dev Sanghvi <devsanghvi06@gmail.com>
2024-04-08 23:04:26 +0530

    gemini-check

---
+++ chat.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+import readline from 'readline';
+dotenv.config();
+import * as fs from "fs"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI=new GoogleGenerativeAI(process.env.API_KEY);
+
+const rl =readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+async function start(){
+    const model=genAI.getGenerativeModel({model:"gemini-pro"});
+
+    const chat=model.startChat({
+        // starting with 0 saved history
+        history:[],
+        generationConfig:{
+            maxOutputTokens:500,
+        },
+    });
+
+    async function askAndRespond(){
+        rl.question("Dev Sanghvi:",async (msg)=>{
+            if(msg.toLowerCase()==="exit"){
+                rl.close();
+            }else{
+                const result=await chat.sendMessage(msg);
+                const response=await result.response;
+                const text=await response.text();
+
+                console.log("Dev's Gemini:",text);
+                askAndRespond();
+
+            }
+        })
+    }
+    askAndRespond();
+}
+
+start()
